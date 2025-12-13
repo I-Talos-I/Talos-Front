10ec50940280a015522fad1b5fc2782d5e313f00
Sergio Cortes <79613492+sergiocortes-dll@users.noreply.github.com>
2025-12-13 03:21:49 -0500

    reload

--- services/index.ts
+++ services/index.ts
@@ -20,6 +20,8 @@ api.interceptors.response.use(
   (response) => response,
   (error) => {
     if (error.response?.status === 401) {
+      if (!localStorage.getItem("token")) return;
+
       localStorage.removeItem("token");
       window.location.href = "/auth/login";
     }
